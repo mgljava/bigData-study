0afccdf9fb2e1b9850f906294783189c312900f2
Minggl <guangliang1120@gmail.com>
2020-05-06 16:23:57 +0800

    [TECH][Minggl]: add hdfs code

--- src/main/java/com/github/mgljava/hdfs/HdfsTest.java
+++ src/main/java/com/github/mgljava/hdfs/HdfsTest.java
@@ -1,8 +1,35 @@
 package com.github.mgljava.hdfs;
 
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+
 public class HdfsTest {
 
-  public static void main(String[] args) {
-    System.out.println("hello");
+  public static void main(String[] args) throws IOException {
+    FileSystem fileSystem = FileSystem.get(new Configuration());
+    FSDataInputStream dataInputStream = fileSystem.open(new Path("/aa/kms.sh"));
+    System.out.println((char) dataInputStream.readByte());
+    System.out.println((char) dataInputStream.readByte());
+    System.out.println((char) dataInputStream.readByte());
+    System.out.println((char) dataInputStream.readByte());
+    System.out.println((char) dataInputStream.readByte());
+
+    long defaultBlockSize = fileSystem.getDefaultBlockSize(new Path("/aa/kms.sh"));
+    System.out.println(defaultBlockSize);
+    System.out.println("---------------");
+    RemoteIterator<LocatedFileStatus> locatedFileStatusRemoteIterator = fileSystem.listFiles(new Path("/software"), true);
+    while (locatedFileStatusRemoteIterator.hasNext()) {
+      LocatedFileStatus next = locatedFileStatusRemoteIterator.next();
+      BlockLocation[] blockLocations = next.getBlockLocations();
+      for (BlockLocation b : blockLocations) {
+        System.out.println(b);
+      }
+    }
   }
 }
