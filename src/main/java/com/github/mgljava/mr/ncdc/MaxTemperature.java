8cb404672572cb90ce68a727c69b9df2a0d23e92
Minggl <guangliang.ming@baozun.com>
2020-05-21 14:18:40 +0800

    [TECH][Minggl]: add code

--- src/main/java/com/github/mgljava/mr/ncdc/MaxTemperature.java
+++ src/main/java/com/github/mgljava/mr/ncdc/MaxTemperature.java
@@ -1,7 +1,36 @@
 package com.github.mgljava.mr.ncdc;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  * 统计 NCDC 每年的最大气温
  */
 public class MaxTemperature {
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperature<input path> <output path>");
+      System.exit(-1);
+    }
+
+    Job job = Job.getInstance(new Configuration(true));
+
+    job.setJarByClass(MaxTemperature.class);
+    job.setJobName("MaxTemperature");
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setReducerClass(MaxTemperatureReduce.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+  }
 }
--- src/main/java/com/github/mgljava/mr/ncdc/MaxTemperatureReduce.java
+++ src/main/java/com/github/mgljava/mr/ncdc/MaxTemperatureReduce.java
@@ -1,4 +1,18 @@
 package com.github.mgljava.mr.ncdc;
 
-public class MaxTemperatureReduce {
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MaxTemperatureReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+  @Override
+  protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    int maxValue = Integer.MAX_VALUE;
+    for (IntWritable value : values) {
+      maxValue = Math.max(maxValue, value.get());
+    }
+    context.write(key, new IntWritable(maxValue));
+  }
 }
