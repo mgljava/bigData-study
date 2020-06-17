3427bdf814a9f10c5077f11771418725b83c8cfd
Minggl <guangliang1120@gmail.com>
2020-06-17 14:45:04 +0800

    [TECH][Minggl]: add zookeeper code

--- src/main/java/com/github/mgljava/zookeeper/ListGroup.java
+++ src/main/java/com/github/mgljava/zookeeper/ListGroup.java
@@ -1,6 +1,7 @@
 package com.github.mgljava.zookeeper;
 
 import java.util.List;
+import org.apache.zookeeper.AsyncCallback.StatCallback;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Stat;
 
@@ -13,12 +14,23 @@ public class ListGroup extends ConnectionWatcher {
     listGroup.listGroup("zookeeper");
 
     listGroup.exists("zookeeper");
+
+    System.out.println("--------------------");
+    listGroup.exists("zookeeper", (rc, path, ctx, stat) -> {
+      System.out.println("rc: " + rc);
+      System.out.println("path: " + path);
+    });
+    Thread.currentThread().join();
   }
 
   // exists
   public void exists(String groupName) throws KeeperException, InterruptedException {
     Stat stat = zk.exists("/" + groupName, false);
     System.out.println(stat.getVersion());
+  }
+
+  public void exists(String groupName, StatCallback callback) {
+    zk.exists("/" + groupName, false, callback, null);
   }
 
   public void listGroup(String groupName) throws KeeperException, InterruptedException {
