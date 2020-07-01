0f57318aed7e4c311ce54fc7b14ce1103cafa1c2
Minggl <guangliang1120@gmail.com>
2020-07-01 14:19:00 +0800

    [TECH][Minggl]: RDD 操作

--- doc/spark/Spark快速大数据分析读书笔记.md
+++ doc/spark/Spark快速大数据分析读书笔记.md
@@ -17,17 +17,34 @@ Spark 中的 RDD 就是一个不可变的分布式对象集合。每个 RDD 都�
 
 ##### RDD操作
 1. 转化操作(transformation)：转化操作会由一个RDD生成一个新的RDD，惰性求值
-  - map操作
-  - filter操作
+  - map操作：将函数应用于 RDD 中的每个元 素，将返回值构成新的 RDD
+  - flatMap()：将函数应用于 RDD 中的每个元 素，将返回的迭代器的所有内 容构成新的 RDD。通常用来切 分单词
+  - filter操作：返回一个由通过传给 filter() 的函数的元素组成的 RDD
+  - distinct()： 去重
   - union(other) 并集，重复元素不会删除
   - intersection(other) 交集，获取两个RDD中都有的元素，需要数据混洗
   - subtract(other)：移除数据，返回 一个由只存在于第一个 RDD 中而不存在于第二个 RDD 中的所有元素组成的 RDD，需要数据混洗
   - cartesian(other) ：计算两个 RDD 的笛卡儿积
+  
 2. 行动操作(action)：行动操作会对RDD计算出一个结果，并把结果返回到驱动器程序或者存储到外部存储系统(如HDFS)
   - count操作
   - first操作
-  - take(num:Int)
+  - take(num:Int)：返回 RDD 中的 n 个元素
+  - reduce()：它接收一个函数作为参数，这个 函数要操作两个 RDD 的元素类型的数据并返回一个同样类型的新元素
+  - aggregate
+  - collect(): 要求所有数据都必须能一同放入单台机器的内存中,在数据量大的情况下不适用
+  - top() ：从 RDD 中获取前几个元素
+  - sample(withRe placement, fra ction, [seed])：对 RDD 采样，以及是否替换
+  - takeSample(withReplacement, num, seed):函数可以让我们从数据中获取一个采样，并指定是否替换。
+  - foreach(): 对 RDD 中的每个元 素进行操作，而不需要把 RDD 发回本地。
+  - countByValue():各元素在 RDD 中出现的次数
+  - takeOrdered(num) (ordering):从 RDD 中按照提供的顺序返 回最前面的 num 个元素
 3. 惰性求值: RDD 的转化操作都是惰性求值的。这意味着在被调用行动操作之前 Spark 不会 开始计算。
+
+##### 数据持久化(缓存)
+如果要缓存的数据太多，内存中放不下，Spark 会自动利用最近最少使用(LRU)的缓存 策略把最老的分区从内存中移除
+1. persist(): persist() 会把数据以序列化的形式缓存在 JVM 的堆空间中
+2. unpersist(): 调用该方法可以手动把持久化的 RDD 从缓 存中移除。
 
 ##### Spark计算数据流程
 1. 从外部数据创建出输入RDD
--- src/main/java/com/github/mgljava/spark/rdd/RDDTest.scala
+++ src/main/java/com/github/mgljava/spark/rdd/RDDTest.scala
@@ -1,5 +1,6 @@
 package com.github.mgljava.spark.rdd
 
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -9,11 +10,35 @@ object RDDTest {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("RDDTest").setMaster("local")
     val sc = new SparkContext(conf)
-    val v1 = sc.parallelize(List(1, 2, 3))
+    val v1 = sc.parallelize(List(1, 2, 3, 2, 3, 1, 8, 19))
     val v2 = sc.parallelize(List(5, 6, 7))
     val value = v1.cartesian(v2)
-    value.foreach(item => {
-      println(item._1 + ":" + item._2)
-    })
+    // value.foreach(item => {
+    // println(item._1 + ":" + item._2)
+    // })
+    val i = v1.reduce((a: Int, b: Int) => a + b)
+    println(i)
+    val i1 = v2.fold(5)((a, b) => a + b)
+    println(i1)
+
+    /**
+     * aggregate
+     */
+    val tuple = v1.aggregate((0, 0))(
+      (acc, value) => (acc._1 + value, acc._2 + 1),
+      (acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+    val i2 = tuple._1 / tuple._2
+    println(i2)
+
+    v1.top(2).foreach(println)
+    println("-----take------")
+    v1.takeSample(withReplacement = true, num = 2).foreach(println)
+    v1.foreach(println)
+
+    println("-----cache------")
+    val value1 = sc.parallelize(List(2, 4, 6))
+    value1.persist(StorageLevel.DISK_ONLY)
+    println(value1.count())
+    println(value1.collect().mkString(";"))
   }
 }
