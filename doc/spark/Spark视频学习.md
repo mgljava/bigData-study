c7ce73a21d09084ef8bf63e8d7fb2b00bee8e7c3
Minggl <guangliang1120@gmail.com>
2020-07-02 17:34:44 +0800

    [TECH][Minggl]: rdd 持久化

--- doc/spark/Spark视频学习.md
+++ doc/spark/Spark视频学习.md
@@ -13,4 +13,19 @@ RDD(Resilient Distributed Dataset), 弹性分布式数据集
 5. partition对外提供最佳的计算位置，利于数据处理的本地化
 
 ##### 算子
-1. 理解为函数，例如 map、flatmap、count等等都是算子
+理解为函数，例如 map、flatmap、count等等都是算子
+
+##### RDD 的持久化
+1. cache：将数据缓存在内存中，懒执行算子，需要action触发
+2. persist: 手动指定持久化级别(StorageLevel)，懒执行算子，需要action触发
+3. checkpoint的功能：懒执行
+  - 将数据存在磁盘中
+  - 切断RDD之间的依赖，可以通过checkpoint的数据进行恢复
+  - checkpoint的数据可以永久存储到磁盘上，而cache和persist在程序运行完了就销毁了
+  - 有些场景必须 checkpoint
+  - 对某个rdd进行checkpoint之前最好cache前一个rdd，避免从头进行计算
+4. cache和persist的注意点
+  - cache和persist都是懒执行，需要action触发
+  - 对一个RDD进行cache或是persist之后可以赋值给一个RDD变量，这个变量就是持久化后的变量
+  - 这个变量后不能直接跟 action算子，如 rdd1.cache().count()
+5. cache、persist、checkpoint持久化的单位都是 partition
---
+++ src/main/java/com/github/mgljava/spark/rdd/CacheTest.scala
@@ -0,0 +1,38 @@
+package com.github.mgljava.spark.rdd
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 持久化算子
+ * cache() 默认将数据存在内存中
+ * persist(StorageLevel.DISK_ONLY) 手动指定持久化级别(StorageLevel)
+ * checkpoint 将数据存在磁盘中 sc.setCheckpointDir("./checkpoint") 设置checkpoint的路径，可以指定为HDFS路径
+ */
+object CacheTest {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("CacheTest")
+    val sc = new SparkContext(conf)
+    sc.setCheckpointDir("./data/checkpoint") // 设置数据的放置路径
+    val lines = sc.textFile("./data/spark/words")
+
+    // lines.cache()
+    // lines.persist(StorageLevel.DISK_ONLY)
+    // lines.persist() == cache()
+    /*
+        val startTime1 = System.currentTimeMillis();
+        val result1 = lines.count()
+        val endTime1 = System.currentTimeMillis()
+        println("count = " + result1 + ", time = " + (endTime1- startTime1) + "ms")
+
+        val startTime2 = System.currentTimeMillis();
+        val result2 = lines.count()
+        val endTime2 = System.currentTimeMillis();
+        println("count = " + result2 + ", time = " + (endTime2- startTime2) + "ms")
+    */
+
+    // checkpoint
+    lines.checkpoint()
+    println(lines.count())
+    sc.stop()
+  }
+}
