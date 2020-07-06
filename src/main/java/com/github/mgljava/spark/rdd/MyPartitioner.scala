0e0e57d14ba127cf504bb5cfb0e5f42349d7b0c6
Minggl <guangliang1120@gmail.com>
2020-07-06 13:43:10 +0800

    [TECH][Minggl]: rdd partition

--- src/main/java/com/github/mgljava/spark/rdd/MyPartitioner.scala
+++ src/main/java/com/github/mgljava/spark/rdd/MyPartitioner.scala
@@ -1,5 +1,28 @@
 package com.github.mgljava.spark.rdd
 
-object MyPartitioner {
+import java.net.URL
 
+import org.apache.spark.Partitioner
+
+/**
+ * 基于域名的分区器
+ */
+class MyPartitioner(number: Int) extends Partitioner {
+  override def numPartitions: Int = number
+
+  override def getPartition(key: Any): Int = {
+    val domain = new URL(key.toString).getHost
+    val code = domain.hashCode % numPartitions
+    if (code < 0) {
+      code + numPartitions // 使其非负
+    }
+    else {
+      code
+    }
+  }
+
+  override def equals(obj: Any): Boolean = {
+    case my: MyPartitioner => my.numPartitions == numPartitions
+    case _ => false
+  }
 }
