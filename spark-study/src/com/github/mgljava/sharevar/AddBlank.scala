1d74fd39b475ca528e39bfd52ae7108911a5dcb2
Minggl <guangliang1120@gmail.com>
2020-07-08 12:05:15 +0800

    [TECH][Minggl]: Spark 累加器

---
+++ spark-study/src/com/github/mgljava/sharevar/AddBlank.scala
@@ -0,0 +1,30 @@
+package com.github.mgljava.sharevar
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 在 Scala 中累加空行
+ */
+object AddBlank {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("AddBlank")
+    val sc = new SparkContext(conf)
+
+    val file = sc.textFile("./data/spark/files/shareva.txt")
+    val blankLines = sc.accumulator(0) // 空行
+    val valueLines = sc.accumulator(0) // 有值的行
+    val callSigns = file.flatMap(line => {
+      if (line == "") {
+        blankLines += 1
+        ""
+      } else {
+        valueLines += 1
+        line.split(" ")
+      }
+    })
+    callSigns.foreach(println)
+    println("-------------")
+    println("Blank lines: " + blankLines.value)
+    println("valueLines lines: " + valueLines.value)
+  }
+}
