cfe1f738ad72b9c52eb270137e156bf25b5abea5
Minggl <guangliang1120@gmail.com>
2020-07-16 17:20:03 +0800

    [TECH][Minggl]: spark sql

--- spark-study/src/main/java/com/github/mgljava/sparksql/SparkSqlTest1.scala
+++ spark-study/src/main/java/com/github/mgljava/sparksql/SparkSqlTest1.scala
@@ -2,6 +2,11 @@ package com.github.mgljava.sparksql
 
 import org.apache.spark.sql.SparkSession
 
+/**
+ * DataFrame 有数据、有列的schema
+ * sqlContext 读取json文件加载成DataFrame，DataFrame会按照 ascii码排序
+ * 写SQL查询出来的DataFrame会按照指定的列显示，不会排序
+ */
 object SparkSqlTest1 {
 
   def main(args: Array[String]): Unit = {
@@ -20,6 +25,11 @@ object SparkSqlTest1 {
     // dataFrame.filter("age>18").show()
 
     // dataFrame.groupBy("age").count().show()
+
+    /*
+     * 将DataFrame注册成临时表
+     * people 这张表不在内存中也不再磁盘中，相当于一个指针指向源文件，底层操作解析spark job 读取源文件
+     */
     dataFrame.createOrReplaceTempView("people")
     val sqlDf = spark.sql("select * from people where age>18")
     sqlDf.show()
