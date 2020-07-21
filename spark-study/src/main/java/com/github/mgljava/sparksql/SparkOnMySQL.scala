0533dce2c0759647a9460c3ab565b3870ca5be79
Minggl <guangliang1120@gmail.com>
2020-07-21 13:56:02 +0800

    [TECH][Minggl]: spark 集成MySQL查询数据

---
+++ spark-study/src/main/java/com/github/mgljava/sparksql/SparkOnMySQL.scala
@@ -0,0 +1,43 @@
+package com.github.mgljava.sparksql
+
+import java.util.Properties
+
+import org.apache.spark.sql.SparkSession
+
+/**
+ * SparkSQL操作MySQL
+ *
+ *
+ */
+object SparkOnMySQL {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local").appName("SparkOnMySQL").getOrCreate()
+
+    // 方式1
+    /*val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/fund")
+      .option("dbtable", "funds")
+      .option("user", "root")
+      .option("password", "123456")
+      .load()*/
+    // 方式2
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", "root")
+    connectionProperties.put("password", "123456")
+    val jdbcDF = spark.read
+      .jdbc("jdbc:mysql://localhost:3306/fund", "funds", connectionProperties)
+    jdbcDF.show()
+    println("-------")
+    val l = jdbcDF.rdd
+      .map(item => item.get(4))
+      .collect()
+    var sum = 0
+    for (i <- l) {
+      sum += Integer.parseInt(i.toString)
+    }
+    println(sum)
+  }
+}
